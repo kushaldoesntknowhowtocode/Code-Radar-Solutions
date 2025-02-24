6e8dead029c359e869268f2c15cb1523fe7b4e83
kushal ahuja <kushalahuja88@gmail.com>
2025-02-24 14:37:41 +0530

    Solution to Check LSB of a Number

---
+++ Check LSB of a Number@@67882a731b4392d8fc1577e8/code.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    // Read the input
+    scanf("%d", &num);
+    // Perform the bitwise one's complement operation and print the result
+    printf("%d\n", ~num);
+    return 0;
+}
