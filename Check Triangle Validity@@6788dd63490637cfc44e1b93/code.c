3f6debd62c8e92cedbb03e10aebe0f8bd516c85b
kushal ahuja <kushalahuja88@gmail.com>
2025-03-16 17:25:28 +0530

    Solution to Check Triangle Validity

---
+++ Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void triangle(int a,int b,int c){
+    if((a^2)+(b^2)==(c^2)){
+        printf("Valid");
+    }else{
+        printf("Invalid");
+    }
+}
+int main(){
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    triangle(a,b,c);
+    return 0;
+}
